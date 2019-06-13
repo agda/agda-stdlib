agda/agda-stdlib

commit 5e8b6aa91adc6d60606e17db7b579be356f72aec

[ fix #185 ] New style reasoning (#586)

src/Relation/Binary/Reasoning/Base/Double.agda
@@ -1,0 +1,115 @@
+------------------------------------------------------------------------
+-- The Agda standard library
+--
+-- The basic code for equational reasoning with two relations:
+-- equality and some other ordering.
+------------------------------------------------------------------------
+--
+-- See `Data.Nat.Properties` or `Relation.Binary.Reasoning.PartialOrder`
+-- for examples of how to instantiate this module.
+
+{-# OPTIONS --without-K --safe #-}
+
+open import Relation.Binary
+
+module Relation.Binary.Reasoning.Base.Double {a ℓ₁ ℓ₂} {A : Set a}
+  {_≈_ : Rel A ℓ₁} {_∼_ : Rel A ℓ₂} (isPreorder : IsPreorder _≈_ _∼_)
+  where
+
+open import Data.Product using (proj₁; proj₂)
+open import Level using (Level; _⊔_; Lift; lift)
+open import Function using (case_of_; id)
+open import Relation.Binary.PropositionalEquality using (_≡_; refl)
+open import Relation.Nullary using (Dec; yes; no)
+open import Relation.Nullary.Decidable using (True; toWitness)
+
+open IsPreorder isPreorder
+
+------------------------------------------------------------------------
+-- A datatype to hide the current relation type
+
+data _IsRelatedTo_ (x y : A) : Set (ℓ₁ ⊔ ℓ₂) where
+  nonstrict : (x∼y : x ∼ y) → x IsRelatedTo y
+  equals    : (x≈y : x ≈ y) → x IsRelatedTo y
+
+levelOf : ∀ {x y} → x IsRelatedTo y → Level
+levelOf (nonstrict x∼y) = ℓ₂
+levelOf (equals    x≈y) = ℓ₁
+
+relOf : ∀ {x y} (r : x IsRelatedTo y) → Rel A (levelOf r)
+relOf (nonstrict x∼y) = _∼_
+relOf (equals    x≈y) = _≈_
+
+------------------------------------------------------------------------
+-- A record that is used to ensure that the final relation proved by the
+-- chain of reasoning can be converted into the required relation.
+
+data IsEquality {x y} : x IsRelatedTo y → Set ℓ₁ where
+  isEquality : ∀ x≈y → IsEquality (equals x≈y)
+
+IsEquality? : ∀ {x y} (x≲y : x IsRelatedTo y) → Dec (IsEquality x≲y)
+IsEquality? (nonstrict _) = no λ()
+IsEquality? (equals x≈y)  = yes (isEquality x≈y)
+
+extractEquality : ∀ {x y} {x≲y : x IsRelatedTo y} → IsEquality x≲y → x ≈ y
+extractEquality (isEquality x≈y) = x≈y
+
+------------------------------------------------------------------------
+-- Reasoning combinators
+
+infix -1 begin_ begin-equality_
+infixr 0 _∼⟨_⟩_ _≈⟨_⟩_ _≡⟨_⟩_ _≡⟨⟩_
+infix  1 _∎
+
+begin_ : ∀ {x y} (r : x IsRelatedTo y) → x ∼ y
+begin (nonstrict x∼y) = x∼y
+begin (equals    x≈y) = reflexive x≈y
+
+begin-equality_ : ∀ {x y} (r : x IsRelatedTo y) → {s : True (IsEquality? r)} → x ≈ y
+begin-equality_ r {s} = extractEquality (toWitness s)
+
+_∼⟨_⟩_ : ∀ (x : A) {y z} → x ∼ y → y IsRelatedTo z → x IsRelatedTo z
+x ∼⟨ x∼y ⟩ nonstrict y∼z = nonstrict (trans x∼y y∼z)
+x ∼⟨ x∼y ⟩ equals    y≈z = nonstrict (proj₁ ∼-resp-≈ y≈z x∼y)
+
+_≈⟨_⟩_ : ∀ (x : A) {y z} → x ≈ y → y IsRelatedTo z → x IsRelatedTo z
+x ≈⟨ x≈y ⟩ nonstrict y∼z = nonstrict (proj₂ ∼-resp-≈ (Eq.sym x≈y) y∼z)
+x ≈⟨ x≈y ⟩ equals    y≈z = equals    (Eq.trans x≈y y≈z)
+
+_≡⟨_⟩_ : ∀ (x : A) {y z} → x ≡ y → y IsRelatedTo z → x IsRelatedTo z
+x ≡⟨ x≡y ⟩ nonstrict y∼z = nonstrict (case x≡y of λ where refl → y∼z)
+x ≡⟨ x≡y ⟩ equals    y≈z = equals    (case x≡y of λ where refl → y≈z)
+
+_≡⟨⟩_ : ∀ (x : A) {y} → x IsRelatedTo y → x IsRelatedTo y
+x ≡⟨⟩ x≲y = x≲y
+
+_∎ : ∀ x → x IsRelatedTo x
+x ∎ = equals Eq.refl
+
+------------------------------------------------------------------------
+-- Some examples and tests
+
+{-
+private
+  module Examples where
+    postulate
+      u v w y d e : A
+      u≈v : u ≈ v
+      v≡w : v ≡ w
+      w∼y : w ∼ y
+      z≡d : y ≡ d
+      d≈e : d ≈ e
+
+    u∼y : u ∼ y
+    u∼y = begin
+      u ≈⟨ u≈v ⟩
+      v ≡⟨ v≡w ⟩
+      w ∼⟨ w∼y ⟩
+      y ∎
+
+    u≈w : u ≈ w
+    u≈w = begin-equality
+      u ≈⟨ u≈v ⟩
+      v ≡⟨ v≡w ⟩
+      w ∎
+-}

src/Relation/Binary/Reasoning/Base/Triple.agda
@@ -1,0 +1,153 @@
+------------------------------------------------------------------------
+-- The Agda standard library
+--
+-- The basic code for equational reasoning with three relations:
+-- equality, strict ordering and non-strict ordering.
+------------------------------------------------------------------------
+--
+-- See `Data.Nat.Properties` or `Relation.Binary.Reasoning.PartialOrder`
+-- for examples of how to instantiate this module.
+
+{-# OPTIONS --without-K --safe #-}
+
+open import Relation.Binary
+
+module Relation.Binary.Reasoning.Base.Triple {a ℓ₁ ℓ₂ ℓ₃} {A : Set a}
+  {_≈_ : Rel A ℓ₁} {_≤_ : Rel A ℓ₂} {_<_ : Rel A ℓ₃}
+  (isPreorder : IsPreorder _≈_ _≤_)
+  (<-trans : Transitive _<_) (<-resp-≈ : _<_ Respects₂ _≈_) (<⇒≤ : _<_ ⇒ _≤_)
+  (<-≤-trans : Trans _<_ _≤_ _<_) (≤-<-trans : Trans _≤_ _<_ _<_)
+  where
+
+open import Data.Product using (proj₁; proj₂)
+open import Function using (case_of_; id)
+open import Level using (Level; _⊔_; Lift; lift)
+open import Relation.Binary.PropositionalEquality using (_≡_; refl)
+open import Relation.Nullary using (Dec; yes; no)
+open import Relation.Nullary.Decidable using (True; toWitness)
+
+open IsPreorder isPreorder
+  renaming
+  ( reflexive to ≤-reflexive
+  ; trans     to ≤-trans
+  ; ∼-resp-≈  to ≤-resp-≈
+  )
+
+------------------------------------------------------------------------
+-- A datatype to hide the current relation type
+
+data _IsRelatedTo_ (x y : A) : Set (ℓ₁ ⊔ ℓ₂ ⊔ ℓ₃) where
+  strict    : (x<y : x < y) → x IsRelatedTo y
+  nonstrict : (x≤y : x ≤ y) → x IsRelatedTo y
+  equals    : (x≈y : x ≈ y) → x IsRelatedTo y
+
+------------------------------------------------------------------------
+-- Types that are used to ensure that the final relation proved by the
+-- chain of reasoning can be converted into the required relation.
+
+data IsStrict {x y} : x IsRelatedTo y → Set ℓ₃ where
+  isStrict : ∀ x<y → IsStrict (strict x<y)
+
+IsStrict? : ∀ {x y} (x≲y : x IsRelatedTo y) → Dec (IsStrict x≲y)
+IsStrict? (strict    x<y) = yes (isStrict x<y)
+IsStrict? (nonstrict _)   = no λ()
+IsStrict? (equals    _)   = no λ()
+
+extractStrict : ∀ {x y} {x≲y : x IsRelatedTo y} → IsStrict x≲y → x < y
+extractStrict (isStrict x<y) = x<y
+
+data IsEquality {x y} : x IsRelatedTo y → Set ℓ₁ where
+  isEquality : ∀ x≈y → IsEquality (equals x≈y)
+
+IsEquality? : ∀ {x y} (x≲y : x IsRelatedTo y) → Dec (IsEquality x≲y)
+IsEquality? (strict    _) = no λ()
+IsEquality? (nonstrict _) = no λ()
+IsEquality? (equals x≈y)  = yes (isEquality x≈y)
+
+extractEquality : ∀ {x y} {x≲y : x IsRelatedTo y} → IsEquality x≲y → x ≈ y
+extractEquality (isEquality x≈y) = x≈y
+
+------------------------------------------------------------------------
+-- Reasoning combinators
+
+infix -1 begin_ begin-strict_ begin-equality_
+infixr 0 _<⟨_⟩_ _≤⟨_⟩_ _≈⟨_⟩_ _≡⟨_⟩_ _≡⟨⟩_
+infix  1 _∎
+
+begin_ : ∀ {x y} (r : x IsRelatedTo y) → x ≤ y
+begin (strict    x<y) = <⇒≤ x<y
+begin (nonstrict x≤y) = x≤y
+begin (equals    x≈y) = ≤-reflexive x≈y
+
+begin-strict_ : ∀ {x y} (r : x IsRelatedTo y) → {s : True (IsStrict? r)} → x < y
+begin-strict_ r {s} = extractStrict (toWitness s)
+
+begin-equality_ : ∀ {x y} (r : x IsRelatedTo y) → {s : True (IsEquality? r)} → x ≈ y
+begin-equality_ r {s} = extractEquality (toWitness s)
+
+_<⟨_⟩_ : ∀ (x : A) {y z} → x < y → y IsRelatedTo z → x IsRelatedTo z
+x <⟨ x<y ⟩ strict    y<z = strict (<-trans x<y y<z)
+x <⟨ x<y ⟩ nonstrict y≤z = strict (<-≤-trans x<y y≤z)
+x <⟨ x<y ⟩ equals    y≈z = strict (proj₁ <-resp-≈ y≈z x<y)
+
+_≤⟨_⟩_ : ∀ (x : A) {y z} → x ≤ y → y IsRelatedTo z → x IsRelatedTo z
+x ≤⟨ x≤y ⟩ strict    y<z = strict    (≤-<-trans x≤y y<z)
+x ≤⟨ x≤y ⟩ nonstrict y≤z = nonstrict (≤-trans x≤y y≤z)
+x ≤⟨ x≤y ⟩ equals    y≈z = nonstrict (proj₁ ≤-resp-≈ y≈z x≤y)
+
+_≈⟨_⟩_ : ∀ (x : A) {y z} → x ≈ y → y IsRelatedTo z → x IsRelatedTo z
+x ≈⟨ x≈y ⟩ strict    y<z = strict    (proj₂ <-resp-≈ (Eq.sym x≈y) y<z)
+x ≈⟨ x≈y ⟩ nonstrict y≤z = nonstrict (proj₂ ≤-resp-≈ (Eq.sym x≈y) y≤z)
+x ≈⟨ x≈y ⟩ equals    y≈z = equals    (Eq.trans x≈y y≈z)
+
+_≡⟨_⟩_ : ∀ (x : A) {y z} → x ≡ y → y IsRelatedTo z → x IsRelatedTo z
+x ≡⟨ x≡y ⟩ strict    y<z = strict    (case x≡y of λ where refl → y<z)
+x ≡⟨ x≡y ⟩ nonstrict y≤z = nonstrict (case x≡y of λ where refl → y≤z)
+x ≡⟨ x≡y ⟩ equals    y≈z = equals    (case x≡y of λ where refl → y≈z)
+
+_≡⟨⟩_ : ∀ (x : A) {y} → x IsRelatedTo y → x IsRelatedTo y
+x ≡⟨⟩ x≲y = x≲y
+
+_∎ : ∀ x → x IsRelatedTo x
+x ∎ = equals Eq.refl
+
+------------------------------------------------------------------------
+-- Some examples and tests
+
+{-
+private
+  module Examples where
+    postulate
+      u v w x y z d e : A
+      u≈v : u ≈ v
+      v≡w : v ≡ w
+      w<x : w < x
+      x≤y : x ≤ y
+      y<z : y < z
+      z≡d : z ≡ d
+      d≈e : d ≈ e
+
+    u≤y : u ≤ y
+    u≤y = begin
+      u ≈⟨ u≈v ⟩
+      v ≡⟨ v≡w ⟩
+      w ≤⟨ <⇒≤ (<-≤-trans w<x x≤y) ⟩
+      y ∎
+
+    u≤c : u < e
+    u≤c = begin-strict
+      u ≈⟨ u≈v ⟩
+      v ≡⟨ v≡w ⟩
+      w <⟨ w<x ⟩
+      x ≤⟨ x≤y ⟩
+      y <⟨ y<z ⟩
+      z ≡⟨ z≡d ⟩
+      d ≈⟨ d≈e ⟩
+      e ∎
+
+    u≈w : u ≈ w
+    u≈w = begin-equality
+      u ≈⟨ u≈v ⟩
+      v ≡⟨ v≡w ⟩
+      w ∎
+-}

src/Codata/Musical/Colist.agda
@@ -29,6 +29,8 @@ open import Function.Inverse as Inv using (_↔_; _↔̇_; Inverse; inverse)
 open import Level using (_⊔_)
 open import Relation.Binary
 import Relation.Binary.Construct.FromRel as Ind
+import Relation.Binary.Reasoning.Preorder as PreR
+import Relation.Binary.Reasoning.PartialOrder as POR
 open import Relation.Binary.PropositionalEquality as P using (_≡_)
 open import Relation.Nullary
 open import Relation.Nullary.Negation
@@ -458,7 +460,6 @@ Any-∈ {P = P} = record
   antisym (x ∷ p₁) p₂ = x ∷ ♯ antisym (♭ p₁) (tail p₂)
 
 module ⊑-Reasoning where
-  import Relation.Binary.Reasoning.PartialOrder as POR
   private
     open module R {a} {A : Set a} = POR (⊑-Poset A)
       public renaming (_≤⟨_⟩_ to _⊑⟨_⟩_)
@@ -471,7 +472,6 @@ module ⊑-Reasoning where
   where module ⊑P = Poset (⊑-Poset A)
 
 module ⊆-Reasoning where
-  import Relation.Binary.Reasoning.Preorder as PreR
   private
     open module R {a} {A : Set a} = PreR (⊆-Preorder A)
       public renaming (_∼⟨_⟩_ to _⊆⟨_⟩_)

src/Data/Integer/Properties.agda
@@ -26,6 +26,7 @@ open import Level using (0ℓ)
 open import Relation.Binary
 open import Relation.Binary.PropositionalEquality
 import Relation.Binary.Reasoning.PartialOrder as POR
+import Relation.Binary.Reasoning.Preorder as PreR
 open import Relation.Nullary using (yes; no)
 open import Relation.Nullary.Negation using (contradiction)
 import Relation.Nullary.Decidable as Dec
@@ -1033,6 +1034,11 @@ _≤?_ : Decidable _≤_
   ; trans         = ≤-trans
   }
 
+≤-preorder : Preorder _ _ _
+≤-preorder = record
+  { isPreorder = ≤-isPreorder
+  }
+
 ≤-isPartialOrder : IsPartialOrder _≡_ _≤_
 ≤-isPartialOrder = record
   { isPreorder = ≤-isPreorder
@@ -1067,9 +1073,6 @@ _≤?_ : Decidable _≤_
   ; _≤_             = _≤_
   ; isDecTotalOrder = ≤-isDecTotalOrder
   }
-
--- A module for reasoning about the _≤_ relation
-module ≤-Reasoning = POR ≤-poset hiding (_≈⟨_⟩_)
 
 -- Other properties of _≤_
 
@@ -1152,7 +1155,8 @@ suc-mono (+≤+ m≤n) = +≤+ (ℕ.s≤s m≤n)
 +-mono-≤ {m} {n} {i} {j} m≤n i≤j = begin
   m + i ≤⟨ +-monoˡ-≤ i m≤n ⟩
   n + i ≤⟨ +-monoʳ-≤ n i≤j ⟩
-  n + j ∎ where open ≤-Reasoning
+  n + j ∎
+  where open POR ≤-poset
 
 neg-≤-pos : ∀ {m n} → - (+ m) ≤ + n
 neg-≤-pos {zero}  = +≤+ z≤n
@@ -1168,7 +1172,8 @@ m≤m+n : ∀ {m} n → m ≤ m + + n
 m≤m+n {m} n = begin
   m       ≡⟨ sym (+-identityʳ m) ⟩
   m + + 0 ≤⟨ +-monoʳ-≤ m (+≤+ z≤n) ⟩
-  m + + n ∎ where open ≤-Reasoning
+  m + + n ∎
+  where open POR ≤-poset
 
 m-n≤m : ∀ m n → m - + n ≤ m
 m-n≤m m n = ≤-steps-neg n ≤-refl
@@ -1190,13 +1195,15 @@ m-n≤0⇒m≤n {m} {n} m-n≤0 = begin
   m + (- n + n) ≡⟨ sym (+-assoc m (- n) n) ⟩
   (m - n) + n   ≤⟨ +-monoˡ-≤ n m-n≤0 ⟩
   + 0 + n       ≡⟨ +-identityˡ n ⟩
-  n ∎ where open POR ≤-poset
+  n             ∎
+  where open POR ≤-poset
 
 m≤n⇒0≤n-m : ∀ {m n} → m ≤ n → + 0 ≤ n - m
 m≤n⇒0≤n-m {m} {n} m≤n = begin
   + 0   ≡⟨ sym (+-inverseʳ m) ⟩
   m - m ≤⟨ +-monoˡ-≤ (- m) m≤n ⟩
-  n - m ∎ where open POR ≤-poset
+  n - m ∎
+  where open POR ≤-poset
 
 0≤n-m⇒m≤n : ∀ {m n} → + 0 ≤ n - m → m ≤ n
 0≤n-m⇒m≤n {m} {n} 0≤n-m = begin
@@ -1205,7 +1212,8 @@ m≤n⇒0≤n-m {m} {n} m≤n = begin
   n - m + m     ≡⟨ +-assoc n (- m) m ⟩
   n + (- m + m) ≡⟨ cong (_+_ n) (+-inverseˡ m) ⟩
   n + + 0       ≡⟨ +-identityʳ n ⟩
-  n ∎ where open POR ≤-poset
+  n             ∎
+  where open POR ≤-poset
 
 n≤1+n : ∀ n → n ≤ (+ 1) + n
 n≤1+n n = ≤-step ≤-refl
@@ -1286,7 +1294,9 @@ n≤1+n n = ≤-step ≤-refl
   }
 
 <-strictPartialOrder : StrictPartialOrder _ _ _
-<-strictPartialOrder = record { isStrictPartialOrder = <-isStrictPartialOrder }
+<-strictPartialOrder = record
+  { isStrictPartialOrder = <-isStrictPartialOrder
+  }
 
 <-isStrictTotalOrder : IsStrictTotalOrder _≡_ _<_
 <-isStrictTotalOrder = record
@@ -1338,7 +1348,8 @@ n≮n { -[1+ suc n ]} (-≤- n<n) =  contradiction n<n ℕₚ.1+n≰n
 +-mono-< {m} {n} {i} {j} m<n i<j = begin
   sucℤ (m + i) ≤⟨ suc-mono {m + i} (<⇒≤ (+-monoˡ-< i {m} {n} m<n)) ⟩
   sucℤ (n + i) ≤⟨ +-monoʳ-< n i<j ⟩
-  n + j ∎ where open ≤-Reasoning
+  n + j        ∎
+  where open POR ≤-poset
 
 +-mono-≤-< : _+_ Preserves₂ _≤_ ⟶ _<_ ⟶ _<_
 +-mono-≤-< {m} {n} {i} {j} m≤n i<j = ≤-<-trans (+-monoˡ-≤ i m≤n) (+-monoʳ-< n i<j)
@@ -1356,20 +1367,36 @@ m≤pred[n]⇒m<n {m} {n} m≤predn = begin
   + 1 + pred n         ≡⟨ sym (+-assoc (+ 1) -[1+ 0 ] n) ⟩
   (+ 1 + -[1+ 0 ]) + n ≡⟨ cong (_+ n) (+-inverseʳ (+ 1)) ⟩
   + 0 + n              ≡⟨ +-identityˡ n ⟩
-  n ∎ where open ≤-Reasoning
+  n                    ∎
+  where open POR ≤-poset
 
 m<n⇒m≤pred[n] : ∀ {m n} → m < n → m ≤ pred n
 m<n⇒m≤pred[n] {m} {n} m<n = begin
   m             ≡⟨ sym (pred-suc m) ⟩
   pred (sucℤ m) ≤⟨ pred-mono m<n ⟩
-  pred n ∎ where open ≤-Reasoning
+  pred n        ∎
+  where open POR ≤-poset
 
 neg-mono-<-> : -_ Preserves _<_ ⟶ _>_
 neg-mono-<-> {i} {j} i<j = begin
   + 1 - j       ≡⟨ cong (_- j) (neg-involutive (+ 1)) ⟩
   - - + 1 - j   ≡⟨ sym (neg-distrib-+ (- + 1) j) ⟩
   - (- + 1 + j) ≤⟨ neg-mono-≤-≥ (m<n⇒m≤pred[n] i<j) ⟩
-  - i           ∎ where open ≤-Reasoning
+  - i           ∎
+  where open POR ≤-poset
+
+------------------------------------------------------------------------
+-- A specialised module for reasoning about the _≤_ and _<_ relations
+
+module ≤-Reasoning where
+  open import Relation.Binary.Reasoning.Base.Triple
+    ≤-isPreorder
+    (λ {i} → <-trans {i})
+    (resp₂ _<_)
+    <⇒≤
+    (λ {i} → <-≤-trans {i})
+    ≤-<-trans
+    public hiding (_≈⟨_⟩_)
 
 ------------------------------------------------------------------------
 -- DEPRECATED NAMES

src/Data/Nat/Coprimality.agda
@@ -158,10 +158,10 @@ prime⇒coprime (suc (suc m)) p (suc n) _ 1+n<2+m {suc (suc i)}
   ⊥-elim (p _ 2+i′∣2+m)
   where
   i<m : i < m
-  i<m = ≤-pred $ ≤-pred (begin
-    3 + i  ≤⟨ s≤s (∣⇒≤ 2+i∣1+n) ⟩
-    2 + n  ≤⟨ 1+n<2+m ⟩
-    2 + m  ∎)
+  i<m = +-cancelˡ-< 2 (begin-strict
+    2 + i ≤⟨ ∣⇒≤ 2+i∣1+n ⟩
+    1 + n <⟨ 1+n<2+m ⟩
+    2 + m ∎)
     where open ≤-Reasoning
 
   2+i′∣2+m : 2 + toℕ (fromℕ≤ i<m) ∣ 2 + m

src/Data/Nat/Properties.agda
@@ -501,6 +501,15 @@ i+j≡0⇒j≡0 i {j} i+j≡0 = i+j≡0⇒i≡0 j (trans (+-comm j i) (i+j≡0))
 +-cancel-≤ : Cancellative _≤_ _+_
 +-cancel-≤ = +-cancelˡ-≤ , +-cancelʳ-≤
 
++-cancelˡ-< : LeftCancellative _<_ _+_
++-cancelˡ-< x {y} {z} = +-cancelˡ-≤ x ∘ subst (_≤ x + z) (sym (+-suc x y))
+
++-cancelʳ-< : RightCancellative _<_ _+_
++-cancelʳ-< y z y+x<z+x = +-cancelʳ-≤ (suc y) z y+x<z+x
+
++-cancel-< : Cancellative _<_ _+_
++-cancel-< = +-cancelˡ-< , +-cancelʳ-<
+
 ≤-stepsˡ : ∀ {m n} o → m ≤ n → m ≤ o + n
 ≤-stepsˡ zero    m≤n = m≤n
 ≤-stepsˡ (suc o) m≤n = ≤-step (≤-stepsˡ o m≤n)
@@ -1511,18 +1520,18 @@ eq? : ∀ {a} {A : Set a} → A ↣ ℕ → Decidable {A = A} _≡_
 eq? inj = via-injection inj _≟_
 
 ------------------------------------------------------------------------
--- Modules for reasoning about natural number relations
+-- A module for reasoning about the _≤_ and _<_ relations
 
--- A module for reasoning about the _≤_ relation
 module ≤-Reasoning where
-  open import Relation.Binary.Reasoning.PartialOrder
-    (DecTotalOrder.poset ≤-decTotalOrder) public
+  open import Relation.Binary.Reasoning.Base.Triple
+    ≤-isPreorder
+    <-trans
+    (resp₂ _<_)
+    <⇒≤
+    <-transˡ
+    <-transʳ
+    public
     hiding (_≈⟨_⟩_)
-
-  infixr 2 _<⟨_⟩_
-
-  _<⟨_⟩_ : ∀ x {y z} → x < y → y IsRelatedTo z → suc x IsRelatedTo z
-  x <⟨ x<y ⟩ y≤z = suc x ≤⟨ x<y ⟩ y≤z
 
 ------------------------------------------------------------------------
 -- DEPRECATED NAMES

src/Relation/Binary/Reasoning/Base/Single.agda
@@ -1,0 +1,59 @@
+------------------------------------------------------------------------
+-- The Agda standard library
+--
+-- The basic code for equational reasoning with a single relation
+------------------------------------------------------------------------
+
+{-# OPTIONS --without-K --safe #-}
+
+open import Relation.Binary
+
+module Relation.Binary.Reasoning.Base.Single
+  {a ℓ} {A : Set a} (_∼_ : Rel A ℓ)
+  (refl : Reflexive _∼_) (trans : Transitive _∼_)
+  where
+
+open import Relation.Binary.PropositionalEquality as P using (_≡_)
+
+infix  4 _IsRelatedTo_
+infix  3 _∎
+infixr 2 _∼⟨_⟩_ _≡⟨_⟩_ _≡⟨⟩_
+infix  1 begin_
+
+-- This seemingly unnecessary type is used to make it possible to
+-- infer arguments even if the underlying equality evaluates.
+
+data _IsRelatedTo_ (x y : A) : Set ℓ where
+  relTo : (x∼y : x ∼ y) → x IsRelatedTo y
+
+begin_ : ∀ {x y} → x IsRelatedTo y → x ∼ y
+begin relTo x∼y = x∼y
+
+_∼⟨_⟩_ : ∀ x {y z} → x ∼ y → y IsRelatedTo z → x IsRelatedTo z
+_ ∼⟨ x∼y ⟩ relTo y∼z = relTo (trans x∼y y∼z)
+
+_≡⟨_⟩_ : ∀ x {y z} → x ≡ y → y IsRelatedTo z → x IsRelatedTo z
+_ ≡⟨ P.refl ⟩ x∼z = x∼z
+
+_≡⟨⟩_ : ∀ x {y} → x IsRelatedTo y → x IsRelatedTo y
+_ ≡⟨⟩ x∼y = _ ≡⟨ P.refl ⟩ x∼y
+
+_∎ : ∀ x → x IsRelatedTo x
+_∎ _ = relTo refl
+
+{-
+private
+  module Examples where
+    postulate
+      v w y d : A
+      v≡w : v ≡ w
+      w∼y : w ∼ y
+      y≡d : y ≡ d
+
+    u∼y : v ∼ d
+    u∼y = begin
+      v ≡⟨ v≡w ⟩
+      w ∼⟨ w∼y ⟩
+      y ≡⟨ y≡d ⟩
+      d ∎
+-}

src/Relation/Binary/Reasoning/MultiSetoid.agda
@@ -7,21 +7,19 @@
 -- Example use:
 --
 --   open import Data.Maybe
---   import Relation.Binary.Reasoning.MultiSetoid as SetR
+--   open import Relation.Binary.Reasoning.MultiSetoid
 --
 --   begin⟨ S ⟩
---     x
---       ≈⟨ drop-just (begin⟨ setoid S ⟩
---          just x
---            ≈⟨ justx≈mz ⟩
---          mz
---            ≈⟨ mz≈justy ⟩
---          just y ∎) ⟩
---     y
---       ≈⟨ y≈z ⟩
+--     x ≈⟨ drop-just (begin⟨ setoid S ⟩
+--          just x ≈⟨ justx≈mz ⟩
+--          mz     ≈⟨ mz≈justy ⟩
+--          just y ∎)⟩
+--     y ≈⟨ y≈z ⟩
 --     z ∎
 
 {-# OPTIONS --without-K --safe #-}
+
+module Relation.Binary.Reasoning.MultiSetoid where
 
 open import Relation.Binary
 open import Relation.Binary.Reasoning.Setoid as EqR using (_IsRelatedTo_)
@@ -29,20 +27,21 @@ open import Relation.Binary.PropositionalEquality
 
 open Setoid renaming (_≈_ to [_]_≈_)
 
-module Relation.Binary.Reasoning.MultiSetoid where
-
 infix 1 begin⟨_⟩_
-infixr 2 _≈⟨_⟩_ _≡⟨_⟩_
+infixr 2 _≈⟨_⟩_ _≡⟨_⟩_ _≡⟨⟩_
 infix 3 _∎
 
 begin⟨_⟩_ : ∀ {c l} (S : Setoid c l) {x y} → _IsRelatedTo_ S x y → [ S ] x ≈ y
 begin⟨_⟩_ S p = EqR.begin_ S p
-
-_∎ : ∀ {c l} {S : Setoid c l} x → _IsRelatedTo_ S x x
-_∎ {S = S} = EqR._∎ S
 
 _≈⟨_⟩_ : ∀ {c l} {S : Setoid c l} x {y z} → [ S ] x ≈ y → _IsRelatedTo_ S y z → _IsRelatedTo_ S x z
 _≈⟨_⟩_ {S = S} = EqR._≈⟨_⟩_ S
 
 _≡⟨_⟩_ : ∀ {c l} {S : Setoid c l} x {y z} → x ≡ y → _IsRelatedTo_ S y z → _IsRelatedTo_ S x z
 _≡⟨_⟩_ {S = S} = EqR._≡⟨_⟩_ S
+
+_≡⟨⟩_ : ∀ {c l} {S : Setoid c l} x {y} → _IsRelatedTo_ S x y → _IsRelatedTo_ S x y
+_≡⟨⟩_ {S = S} = EqR._≡⟨⟩_ S
+
+_∎ : ∀ {c l} {S : Setoid c l} x → _IsRelatedTo_ S x x
+_∎ {S = S} = EqR._∎ S

commit 6feb1ad9e87d9aa3d6ff8f4fd4922329e36ef853

[ fix #814 ] fix inconsistent fixity of reasoning combinators introduced in #586.

src/Relation/Binary/Reasoning/Base/Double.agda
@@ -28,6 +28,8 @@ open IsPreorder isPreorder
 ------------------------------------------------------------------------
 -- A datatype to hide the current relation type
 
+infix 4 _IsRelatedTo_
+
 data _IsRelatedTo_ (x y : A) : Set (a ⊔ ℓ₁ ⊔ ℓ₂) where
   nonstrict : (x∼y : x ∼ y) → x IsRelatedTo y
   equals    : (x≈y : x ≈ y) → x IsRelatedTo y
@@ -57,9 +59,9 @@ extractEquality (isEquality x≈y) = x≈y
 ------------------------------------------------------------------------
 -- Reasoning combinators
 
-infix -1 begin_ begin-equality_
-infixr 0 _∼⟨_⟩_ _≈⟨_⟩_ _≈˘⟨_⟩_ _≡⟨_⟩_ _≡˘⟨_⟩_ _≡⟨⟩_
-infix  1 _∎
+infix  1 begin_ begin-equality_
+infixr 2 _∼⟨_⟩_ _≈⟨_⟩_ _≈˘⟨_⟩_ _≡⟨_⟩_ _≡˘⟨_⟩_ _≡⟨⟩_
+infix  3 _∎
 
 begin_ : ∀ {x y} (r : x IsRelatedTo y) → x ∼ y
 begin (nonstrict x∼y) = x∼y

src/Relation/Binary/Reasoning/Base/Triple.agda
@@ -36,6 +36,8 @@ open IsPreorder isPreorder
 ------------------------------------------------------------------------
 -- A datatype to hide the current relation type
 
+infix 4 _IsRelatedTo_
+
 data _IsRelatedTo_ (x y : A) : Set (a ⊔ ℓ₁ ⊔ ℓ₂ ⊔ ℓ₃) where
   strict    : (x<y : x < y) → x IsRelatedTo y
   nonstrict : (x≤y : x ≤ y) → x IsRelatedTo y
@@ -70,9 +72,9 @@ extractEquality (isEquality x≈y) = x≈y
 ------------------------------------------------------------------------
 -- Reasoning combinators
 
-infix -1 begin_ begin-strict_ begin-equality_
-infixr 0 _<⟨_⟩_ _≤⟨_⟩_ _≈⟨_⟩_ _≈˘⟨_⟩_ _≡⟨_⟩_ _≡˘⟨_⟩_ _≡⟨⟩_
-infix  1 _∎
+infix  1 begin_ begin-strict_ begin-equality_
+infixr 2 _<⟨_⟩_ _≤⟨_⟩_ _≈⟨_⟩_ _≈˘⟨_⟩_ _≡⟨_⟩_ _≡˘⟨_⟩_ _≡⟨⟩_
+infix  3 _∎
 
 begin_ : ∀ {x y} (r : x IsRelatedTo y) → x ≤ y
 begin (strict    x<y) = <⇒≤ x<y