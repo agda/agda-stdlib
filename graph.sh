7dacebc6e75e0cd34567198779020eef949fb137
Guillaume ALLAIS <guillaume.allais@ens-lyon.org>
2021-02-27 22:54:36 +0000

    [ tools ] generalise & share my graph script

---
+++ graph.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+DIR=$(dirname $1)
+BASE=$(basename $1 ".agda")
+FILE=_build/${DIR}/${BASE}
+mkdir -p _build/$DIR
+agda -isrc/ --dependency-graph=${FILE}.dot $1
+tred ${FILE}.dot > ${FILE}2.dot
+mv ${FILE}2.dot ${FILE}.dot
+dot -Tsvg ${FILE}.dot > ${FILE}.svg
+ln -is ${FILE}.svg ${BASE}.svg
