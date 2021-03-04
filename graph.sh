660a37f86b7f8d21b70256f828a996bf1b30f469
Guillaume ALLAIS <guillaume.allais@ens-lyon.org>
2021-03-04 22:06:35 +0000

    [ graph ] script valid for README & Everything too

--- graph.sh
+++ graph.sh
@@ -3,7 +3,7 @@ DIR=$(dirname $1)
 BASE=$(basename $1 ".agda")
 FILE=_build/${DIR}/${BASE}
 mkdir -p _build/$DIR
-agda -isrc/ --dependency-graph=${FILE}.dot $1
+agda -i. -isrc/ --dependency-graph=${FILE}.dot $1
 tred ${FILE}.dot > ${FILE}2.dot
 mv ${FILE}2.dot ${FILE}.dot
 dot -Tsvg ${FILE}.dot > ${FILE}.svg
