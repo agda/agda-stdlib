a534f6a40044382fde80ea069fce9a5b8d477d41
Guillaume Allais <guillaume.allais@ens-lyon.org>
2023-10-12 16:33:40 +0100

    [ refactor ] use template file + .sh script

---
+++ travis/landing.sh
@@ -0,0 +1,12 @@
+set -eu
+set -o pipefail
+
+find html/ -name "index.html" \
+  | grep -v "master\|experimental" \
+  | sort
+  | sed 's|html/\([^\/]*\)/index.html|  <li><a href="\1">\1</a></li>|g' \
+  >> landing.html
+
+echo "</ul>" >> landing.html
+
+mv landing.html html/index.html
