1a942bfc06a4780f538eeccf8474e026f20eaaba
Guillaume Allais <guillaume.allais@ens-lyon.org>
2023-10-13 10:59:13 +0100

    [ cleanup ] More html structure

--- travis/landing.sh
+++ travis/landing.sh
@@ -3,12 +3,14 @@ set -o pipefail
 
 rm html/index.html
 
+cat landing-top.html >> landing.html
+
 find html/ -name "index.html" \
   | grep -v "master\|experimental" \
   | sort -r \
   | sed 's|html/\([^\/]*\)/index.html|  <li><a href="\1">\1</a></li>|g' \
   >> landing.html
 
-echo "</ul>" >> landing.html
+cat landing-bottom.html >> landing.html
 
 mv landing.html html/index.html
