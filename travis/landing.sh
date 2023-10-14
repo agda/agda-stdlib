eaaab4fc2410affa9f26b02529bf1a00fd08dc69
G. Allais <guillaume.allais@ens-lyon.org>
2023-10-14 08:48:13 +0100

    [ fix #2086 ] new web deployment strategy (#2147)

---
+++ travis/landing.sh
@@ -0,0 +1,17 @@
+set -eu
+set -o pipefail
+
+rm html/index.html
+
+cat landing-top.html >> landing.html
+
+find html/ -name "index.html" \
+  | grep -v "master\|experimental" \
+  | sort -r \
+  | sed 's|html/\([^\/]*\)/index.html|        <li><a href="\1">\1</a></li>|g' \
+  >> landing.html
+
+cat landing-bottom.html >> landing.html
+
+mv landing.html html/index.html
+mv agda-logo.svg html/
