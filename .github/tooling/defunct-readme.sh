69dc9affec7c8067f0bfb9884d4b652100c9f563
Silas Hayes-Williams <psysh13@nottingham.ac.uk>
2026-06-30 14:40:59 -0400

    Update defunct-readme.sh to scan for references to deleted READMEs

--- .github/tooling/defunct-readme.sh
+++ .github/tooling/defunct-readme.sh
@@ -6,9 +6,22 @@
 #
 # If a reference to a now deleted README is found, an error is raised
 
-echo "Searching for deleted READMEs"
+echo "Searching for deleted READMEs..."
 
 git diff --diff-filter D --name-only master | grep '^doc/README/' | while read file; do
     echo "$file was deleted, scanning for references"
+   
+    # remove 'doc/' and '.agda, replace / with .
+    module=${file#"doc/"}
+    module=${module%".agda"}
+    module=${module//\//.} 
+    
+    # search for *any* reference to the README
+    # be it an import, in a comment, or wherever else
+    grep -r ".*$module.*" | while read ref; do
+        echo "ERROR: file referenced: $ref" >&2
+        exit 1
+    done
 done
 
+exit
