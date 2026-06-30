8fca1ec3dacb8fbdb4ead8d6eb422e18ef7a4f4e
Silas Hayes-Williams <psysh13@nottingham.ac.uk>
2026-06-30 15:08:40 -0400

    Update defunct-readme.sh to scan for renamed files as well

--- .github/tooling/defunct-readme.sh
+++ .github/tooling/defunct-readme.sh
@@ -5,10 +5,12 @@
 # for them in existing READMEs
 #
 # If a reference to a now deleted README is found, an error is raised
+#
+# Silas Hayes-Williams, 2026
 
 echo "Searching for deleted READMEs..."
 
-git diff --diff-filter D --name-only master | grep '^doc/README/' | while read file; do
+git diff --diff-filter DR --name-status master | awk '{print $2}' | grep '^doc/README/' | while read file; do
     echo "$file was deleted, scanning for references"
    
     # remove 'doc/' and '.agda, replace / with .
@@ -18,7 +20,14 @@ git diff --diff-filter D --name-only master | grep '^doc/README/' | while read f
     
     # search for *any* reference to the README
     # be it an import, in a comment, or wherever else
-    grep -r ".*$module.*" | while read ref; do
+    # ([^\.]*)?$ says the module must be followed by *not* a . and any other text, or followed
+    # by nothing
+    #
+    # E.g. if README.Axiom was deleted, then it would match
+    #         "-- see README.Axiom for some notes. :-)"
+    #      or "-- see README.Axiom"
+    # but not "-- see README.Axiom.Some.Sub.Mod"
+    grep -r -E ".*$module([^\.].*)?$" | while read ref; do
         echo "ERROR: file referenced: $ref" >&2
         exit 1
     done
