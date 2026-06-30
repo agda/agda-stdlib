723dcfab106a3376bedcbf4921a8ed5ebf6c9aa5
Silas Hayes-Williams <psysh13@nottingham.ac.uk>
2026-06-30 12:17:28 -0400

    Add defunct-readme.sh

---
+++ .github/tooling/defunct-readme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script searches through any READMEs which have been removed from 
+# the current branch (via a diff with master) and scans for references
+# for them in existing READMEs
+#
+# If a reference to a now deleted README is found, an error is raised
+
+echo "Searching for deleted READMEs"
+
+git diff --diff-filter D --name-only master | grep '^doc/README/' | while read file; do
+    echo "$p was deleted, scanning for references"
+done
+
